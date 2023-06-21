eb8584c92f773d3579aec3d12fdc4052e8452748
itaintkevin <kpjtofficial@gmail.com>
2023-06-21 08:44:41 +0530

    feat: Fetching brews on click

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,25 +2,37 @@ import Hero from '@/components/Hero'
 import BrewCoffee from '@/components/BrewCoffee'
 import Brews from '@/components/Brews'
 import Navbar from '@/components/Navbar'
-import { Nunito_Sans } from 'next/font/google'
+import { Space_Mono } from 'next/font/google'
 import Footer from '@/components/Footer'
+import { useState } from 'react'
 
-const nunitoSans = Nunito_Sans({
-  weight: ['200', '300', '400', '600', '700', '800', '900'],
+const spaceMono = Space_Mono({
+  weight: ['400', '700'],
   style: ['normal', 'italic'],
   subsets: ['latin']
 })
 
 export default function Home() {
+  const [fetchBrews, setFetchBrews] = useState(false);
+
+  const handleClick = () => {
+    setFetchBrews(true);
+  }
+
   return (
-    <main className={`bg-primary text-white min-h-screen ${nunitoSans.className}`}>
+    <main className={`bg-primary text-white min-h-screen ${spaceMono.className}`}>
       <section className="max-w-[1024px] mx-auto px-5">
       <Navbar />
       <div className="flex flex-col sm:flex-row justify-between">
         <Hero/>
         <BrewCoffee />
       </div>
-      <Brews />
+      {
+        !fetchBrews && <div className="flex justify-center w-full mt-10">
+          <button onClick={handleClick} className="bg-white text-black text-lg font-bold rounded-lg w-[200px] p-2">Fetch Brews</button>
+        </div>
+      }
+      { fetchBrews && <Brews /> }
       <Footer />
       </section>
     </main>
